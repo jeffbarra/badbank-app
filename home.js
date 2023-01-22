00afa9904973bfd16bb042aba6211fa44335b2df
Jeff Barra <103481560+jeffbarra@users.noreply.github.com>
2023-01-21 17:46:24 -0700

    Add files via upload

---
+++ home.js
@@ -0,0 +1,25 @@
+function Home() {
+  return (
+    <>
+      <div className="hero-container">
+        <div className="hero-background">
+          <div className="hero-content">
+            <h1>Welcome to BadBank</h1>
+            <p>
+              The most <span>unsecure</span> bank in the world!
+            </p>
+          </div>
+        </div>
+      </div>
+      {/* <Card
+        className="home-card"
+        txtcolor="black"
+        title="Welcome to BadBank"
+        text="The most unsecure bank in the world!"
+        body={
+          <img src="money.png" className="img-fluid" alt="Responsive image" />
+        }
+      /> */}
+    </>
+  );
+}
